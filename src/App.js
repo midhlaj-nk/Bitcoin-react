9abdb5454cd5364d77960bbf3d7041156465f672
midhlaj-nk <midhlaj.nk@gmail.com>
2023-01-30 12:54:28 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Coin from "./component/Coin";
 
 function App() {
+  const [listofCoins, SetListOfCoins] = useState([]);
+  const [searchWord, SetSearchWord] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://api.coinstats.app/public/v1/coins?skip=0&")
+      .then((respone) => {
+        SetListOfCoins(respone.data.coins);
+        console.log(respone.data);
+      });
+  }, []);
+
+  const CoinsFiltered = listofCoins.filter((coin) => {
+      return coin.name.toLowerCase().includes(searchWord.toLowerCase());
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+        <ul>
+          <li>Bitcoin Price Tracker</li>
+        </ul>
+        <div>
+          <input
+            type="text"
+            placeholder="Search For Bitcoins Here"
+            onChange={(event) => {
+              SetSearchWord(event.target.value);
+            }}
+          />
+        </div>
+      </div>
+      <div className="Display">
+        {CoinsFiltered.map((coin) => (
+          <div className="box">
+            <Coin
+              name={coin.name}
+              price={coin.price}
+              symbol={coin.symbol}
+              icon={coin.icon}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
